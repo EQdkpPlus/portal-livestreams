bdac533e143b65d897a7e8ab3db534b7292fe37b
Asitara <majsterofdesaster@online.de>
2015-06-27 06:14:41 +0200

    fix links and rewritten JS

--- livestreams_portal.class.php
+++ livestreams_portal.class.php
@@ -109,122 +109,122 @@ class livestreams_portal extends portal_generic {
 		$arrUserIDs = $this->pdh->sort($this->pdh->get('user', 'id_list'), 'user', 'name', 'asc');
 		
 		$this->tpl->add_js('
-			 $(".ls_twitch").each(function (index, streamName) {
-				myStreamName = $(this).data("streamname");
+			$(".ls_status").each(function(){
+				streamName = $(this).data("streamname");
+				streamType = $(this).data("streamtype");
 				
-	            $.ajax({
-	                url: "https://api.twitch.tv/kraken/streams/" + myStreamName,
-	                dataType: "jsonp",
-	                type: "get",
-	                complete: function (jqXHR, textStatus) {
-	                    twitchStreamComplete(myStreamName, jqXHR);
-	                }
-	            });
-	        });
-				
-			function twitchStreamComplete(streamName, resp) {				
-	            if (resp.responseJSON.stream != null) {
-	               $("." + streamName + "_status").html("<i class=\"eqdkp-icon-offline blink_me\" style=\"color:red;\"></i> '.$this->jquery->sanitize($this->user->lang('ls_online')).'");
-	            } else {
-	                $("." + streamName + "_status").html("<i class=\"fa fa-close\" style=\"color:red;\"></i> '.$this->jquery->sanitize($this->user->lang('ls_offline')).'");
-	            }
-	        }
-				
-			$(".ls_hb").each(function (index, streamName) {
-				myStreamName = $(this).data("streamname");
-				
-	             $.ajax({
-	                url: "https://api.hitbox.tv/media/status/" + myStreamName + ".json",
+				switch(streamType){
+					case "twitch":
+						twitchStreamCheck(streamName);
+						break;
+					
+					case "hb":
+						hbStreamCheck(streamName);
+						break;
+				}
+			});
+			
+			function twitchStreamCheck(streamName){
+				$.ajax({
+					url: "https://api.twitch.tv/kraken/streams/" + streamName,
+					dataType: "jsonp",
+					type: "get",
+					complete: function(resp, textStatus){
+						
+						streamStatus = (resp.responseJSON.stream != null)? true : false;
+						lsStreamResponse(streamName, "twitch", streamStatus);
+					}
+				});
+			}
+			
+			function hbStreamCheck(streamName){
+				$.ajax({
+					url: "https://api.hitbox.tv/media/status/" + streamName + ".json",
 					dataType: "json",
-	                type: "get",
-	                complete: function (jqXHR, textStatus) {
-	                    hbStreamComplete(myStreamName, jqXHR);
-	                }
-	            });
-	        });
-				
-			function twitchStreamComplete(streamName, resp) {				
-	            if (resp.responseJSON.stream != null) {
-	               $("." + streamName + "_status").html("<i class=\"eqdkp-icon-offline blink_me\" style=\"color:red;\"></i> '.$this->jquery->sanitize($this->user->lang('ls_online')).'");
-	            } else {
-	                $("." + streamName + "_status").html("<i class=\"fa fa-close\" style=\"color:red;\"></i> '.$this->jquery->sanitize($this->user->lang('ls_offline')).'");
-	            }
-	        }
-				
-			function hbStreamComplete(streamName, resp) {				
-	            if (resp.responseJSON.media_is_live == "1") {
-	               $("." + streamName + "_status").html("<i class=\"eqdkp-icon-offline blink_me\" style=\"color:red;\"></i> '.$this->jquery->sanitize($this->user->lang('online')).'");
-	            } else {
-	                $("." + streamName + "_status").html("<i class=\"fa fa-close\" style=\"color:red;\"></i> '.$this->jquery->sanitize($this->user->lang('offline')).'");
-	            }
-	        }	
-				
+					type: "get",
+					complete: function(resp, textStatus){
+						
+						streamStatus = (resp.responseJSON.media_is_live == "1")? true : false;
+						lsStreamResponse(streamName, "hb", streamStatus);
+					}
+				});
+			}
+			
+			function lsStreamResponse(streamName, streamType, streamStatus){
+				if(streamStatus){
+					$(".ls_status." + streamType + "_" + streamName).html("<i class=\"eqdkp-icon-offline blink_me\" style=\"color:red;\"></i> '.$this->jquery->sanitize($this->user->lang('ls_online')).'");
+				}else{
+					$(".ls_status." + streamType + "_" + streamName).html("<i class=\"fa fa-close\" style=\"color:red;\"></i> '.$this->jquery->sanitize($this->user->lang('ls_offline')).'");
+				}
+			}
 		', 'docready');
 		
-		$this->tpl->add_css(".blink_me {
-    -webkit-animation-name: blinker;
-    -webkit-animation-duration: 3s;
-    -webkit-animation-timing-function: linear;
-    -webkit-animation-iteration-count: infinite;
-
-    -moz-animation-name: blinker;
-    -moz-animation-duration: 3s;
-    -moz-animation-timing-function: linear;
-    -moz-animation-iteration-count: infinite;
-
-    animation-name: blinker;
-    animation-duration: 3s;
-    animation-timing-function: linear;
-    animation-iteration-count: infinite;
-}
-
-@-moz-keyframes blinker {  
-    0% { opacity: 1.0; }
-    50% { opacity: 0.0; }
-    100% { opacity: 1.0; }
-}
-
-@-webkit-keyframes blinker {  
-    0% { opacity: 1.0; }
-    50% { opacity: 0.0; }
-    100% { opacity: 1.0; }
-}
-
-@keyframes blinker {  
-    0% { opacity: 1.0; }
-    50% { opacity: 0.0; }
-    100% { opacity: 1.0; }
-}");
-
+		$this->tpl->add_css("
+			.blink_me {
+				-webkit-animation-name: blinker;
+				-webkit-animation-duration: 3s;
+				-webkit-animation-timing-function: linear;
+				-webkit-animation-iteration-count: infinite;
+			
+				-moz-animation-name: blinker;
+				-moz-animation-duration: 3s;
+				-moz-animation-timing-function: linear;
+				-moz-animation-iteration-count: infinite;
+			
+				animation-name: blinker;
+				animation-duration: 3s;
+				animation-timing-function: linear;
+				animation-iteration-count: infinite;
+			}
+			
+			@-moz-keyframes blinker {  
+				0% { opacity: 1.0; }
+				50% { opacity: 0.0; }
+				100% { opacity: 1.0; }
+			}
+			
+			@-webkit-keyframes blinker {  
+				0% { opacity: 1.0; }
+				50% { opacity: 0.0; }
+				100% { opacity: 1.0; }
+			}
+			
+			@keyframes blinker {  
+				0% { opacity: 1.0; }
+				50% { opacity: 0.0; }
+				100% { opacity: 1.0; }
+			}
+		");
+		
 		foreach($arrUserIDs as $intUserID){
+			
 			$strTwitch = $this->pdh->get('user', 'profilefield_by_name', array($intUserID, 'twitch'));
 			if($strTwitch && $strTwitch != ""){
-					
+				
 				$strUsername = $this->pdh->get('user', 'name', array($intUserID));
-			
+				
 				$this->tpl->assign_block_vars('ls_user_row', array(
 					'USERNAME' 		=> $strUsername,
 					'USERLINK' 		=> $this->routing->build('user', $strUsername, 'u'.$intUserID),
 					'STREAM_TYPE'	=> 'twitch',
 					'STREAM_NAME'	=> '<i class="fa fa-twitch" title="Twitch"></i>',
-					'STREAM_LINK'	=> 'http://www.twitch.tv/'.utf8_strtolower($strTwitch),
-					'STREAM_USERNAME' => sanitize(utf8_strtolower($strTwitch)),
+					'STREAM_LINK'	=> utf8_strtolower($strTwitch),
+					'STREAM_USERNAME' => str_replace('http://www.twitch.tv/', '', sanitize(utf8_strtolower($strTwitch))),
 				));
 			}
 			
 			$strHitbox = $this->pdh->get('user', 'profilefield_by_name', array($intUserID, 'hitbox'));
-			
 			if($strHitbox && strlen($strHitbox)){
-			
+				
 				$strUsername = $this->pdh->get('user', 'name', array($intUserID));
-			
+				
 				$this->tpl->assign_block_vars('ls_user_row', array(
 					'USERNAME' 		=> $strUsername,
 					'USERLINK' 		=> $this->routing->build('user', $strUsername, 'u'.$intUserID),
 					'STREAM_TYPE'	=> 'hb',
 					'STREAM_NAME'	=> 'Hitbox',
-					'STREAM_LINK'	=> 'http://www.hitbox.tv/'.utf8_strtolower($strHitbox),
-					'STREAM_USERNAME' => sanitize(utf8_strtolower($strHitbox)),
+					'STREAM_LINK'	=> utf8_strtolower($strHitbox),
+					'STREAM_USERNAME' => str_replace('http://www.hitbox.tv/', '', sanitize(utf8_strtolower($strHitbox))),
 				));
 			}
 		}
